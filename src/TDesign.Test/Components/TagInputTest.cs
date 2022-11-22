56b04fb3a37f65c631f9f9baa6ca613090f4e3ee
William <william-zzh@outlook.com>
2022-11-22 11:14:46 +0800

    [feat] init component

---
+++ src/TDesign.Test/Components/TagInputTest.cs
@@ -0,0 +1,25 @@
+﻿using ComponentBuilder;
+
+namespace TDesign.Test.Components;
+public class TagInputTest : TestBase<TTagInput>
+{
+    [Fact(DisplayName = "TagInput - 渲染组件")]
+    public void Test_Render()
+    {
+        GetComponent()
+            .MarkupMatches(builder =>
+            {
+                builder.Div().Class("t-input__wrap t-tage-input").Content(content =>
+                {
+                    content.Div().Class("t-input t-size-m t-is-default t-input --prefix")
+                            .Content(input =>
+                            {
+                                input.Div().Class("t-input__prefix").Close();
+                                input.Open("input").Class("t-input__iner").Attributes(("type", "text")).Close();
+                                input.Span().Class("t-input__input-pre").Close();
+                            })
+                            .Close();
+                }).Close();
+            });
+    }
+}
---
+++ src/TDesign/Components/Forms/TTagInput.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+
+namespace TDesign;
+/// <summary>
+/// 标签输入框。用于输入文本标签。
+/// </summary>
+public class TTagInput : BlazorInputComponentBase<string[]>
+{
+    /// <summary>
+    /// 设置标签多时换行显示。
+    /// </summary>
+    [Parameter] public bool Breakline { get; set; }
+
+    protected override void AddContent(RenderTreeBuilder builder, int sequence)
+    {
+        builder.CreateElement(sequence, "div", tag =>
+        {
+            builder.CreateComponent<TInputText<string[]>>(0, attributes: new
+            {
+                Value,
+                ValueExpression,
+                ValueChanged,
+
+            });
+        },
+        new
+        {
+            @class = HtmlHelper.CreateCssBuilder()
+                            .Append("t-input__wrap t-tag-input")
+                            .Append("t-tag-input--break-line", Breakline)
+        });
+    }
+
+    void BuildTag()
+    {
+
+    }
+}
